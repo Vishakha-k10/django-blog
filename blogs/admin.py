ebfb548e525eee99e7082fa2845e059ec9c81f9c
Vishakha Kumrawat <vishakhakumrawat340@gmail.com>
2026-03-07 16:19:12 +0530

    created blogs app and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from  .models import Category
+from  .models import Blog
+class  BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields={'slug': ('title',)}
+    list_display=('title','author','category','status','is_featured','created_at')
+    search_fields=('title','author__username','category__category_name','status')
+    list_editable=('status','is_featured')
+
+
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
+
+# Register your models here.

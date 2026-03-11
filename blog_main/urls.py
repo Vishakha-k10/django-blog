126ba448c4a5791f4fcd77fce3cefd0bcbfada07
Vishakha Kumrawat <vishakhakumrawat340@gmail.com>
2026-03-11 23:23:28 +0530

    Registration And Login Functionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -26,7 +26,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home , name='home'),
     path('category/',include('blogs.urls')),
-    path('<slug:slug>/', blogviews.blogs , name='blogs'),
+    path('blogs/<slug:slug>/', blogviews.blogs , name='blogs'),
     #serach endpoint
     path('blogs/search/', blogviews.search , name='search'),
+    path('register/', views.register , name='register'),
+    path('login/', views.login ,  name='login'),
+    path('logout/', views.logout ,  name='logout')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
